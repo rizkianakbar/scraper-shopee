17ff460fd17c23dbc946e476c24d8b73dda4d5d5
fauzanrozzaqo <rizkianakbar4@gmail.com>
2021-08-02 07:34:34 +0700

    🗑️REMOVE : unused codes

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-export const root = (req: Request, res: Response) => {
-	return res.status(200).json({
-		status: 'OK',
-		message: 'Welcome to scraper API',
-	});
-};
--- src/routes/products/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { getProductDetails, getSearchProduct } from './controller';
-
-const productRouter = Router();
-productRouter.post('/products', getProductDetails);
-productRouter.post('/products/search-items', getSearchProduct);
-export default productRouter;
